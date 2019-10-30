065aab2b5e54179450fdb0335dff7a4977aab016
guruu.singh2033 <guruu.singh2033@gmail.com>
2019-10-30 18:32:14 +0530

    add comment

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
-const Cryptr = require('cryptr');
-const cryptr = new Cryptr('myTotalySecretKey');
+
+// Code Start : encrypt decrypt
+	const Cryptr = require('cryptr');
+	const cryptr = new Cryptr('myTotalySecretKey');
  
-const encryptedString = cryptr.encrypt('4567');
-const decryptedString = cryptr.decrypt(encryptedString);
+	const encryptedString = cryptr.encrypt('4567');
+	const decryptedString = cryptr.decrypt(encryptedString);
+	
+	console.log("encrypt ---------  ", encryptedString);
+	console.log("decrypt ---------  ", decryptedString); // bacon
+// Code END : encrypt decrypt
  
-console.log("encrypt ---------  ", encryptedString);
-console.log("decrypt ---------  ", decryptedString); // bacon
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
